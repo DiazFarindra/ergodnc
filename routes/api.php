29478054b6aa4abd1b426b53b06a401fb1a77ed0
diaz farindra <farindrad@gmail.com>
2022-01-25 13:32:57 +0700

    add: add new office image controller route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\{
     OfficeController,
+    OfficeImageController,
     TagController
 };
 
@@ -19,12 +20,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// tags
+// tags...
 Route::get('/tags', TagController::class);
 
-// offices
+// offices...
 Route::get('/offices', [OfficeController::class, 'index']);
 Route::get('/offices/{office}', [OfficeController::class, 'show']);
 Route::post('/offices', [OfficeController::class, 'store'])->middleware(['auth:sanctum', 'verified']);
 Route::put('/offices/{office}', [OfficeController::class, 'update'])->middleware(['auth:sanctum', 'verified']);
 Route::delete('/offices/{office}', [OfficeController::class, 'destroy'])->middleware(['auth:sanctum', 'verified']);
+
+// offices photos...
+Route::put('/offices/{office}/images', [OfficeImageController::class, 'store'])->middleware(['auth:sanctum', 'verified']);
+Route::delete('/offices/{office}/images/{image}', [OfficeImageController::class, 'destroy'])->middleware(['auth:sanctum', 'verified']);
